ebf1e16f33ad9d8a7b927f37c030557e1bf88eea
Albus-489 <smartoctopusz@gmail.com>
2022-12-28 19:32:12 +0200

    RMQ start

---
+++ SharedProject/SharedProjRMQ/Models/AuthorModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedProjRMQ.Models
+{
+    public class AuthorModel
+    {
+        public string Id { get; set; }
+        public DateTime? birth { get; set; }
+        public DateTime? death { get; set; }
+        public int? rating { get; set; }
+        public string? bio { get; set; }
+    }
+}
